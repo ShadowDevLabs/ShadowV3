d9818c5086f1e44706a27c731bb3f2c80a32b13e
NCCoder2 <133831293+NCCoder2@users.noreply.github.com>
2023-11-18 20:53:01 +0000

    S

--- public/script.js
+++ public/script.js
@@ -52,7 +52,6 @@ window.addEventListener('load', function() {
     const iframe = document.createElement("iframe");
     iframe.src = url;
     iframesContainer.appendChild(iframe);
-  
     switchTab(tabs.indexOf(tab)); 
   }
   
@@ -208,3 +207,104 @@ window.addEventListener('load', function() {
     });
   });
   
+// Menu Buttons
+
+function iframefullscreen() {
+  const iframe = iframesContainer.children[currentTabIndex];
+  if (iframe.requestFullscreen) {
+    iframe.requestFullscreen();
+  } else if (iframe.mozRequestFullScreen) {
+    iframe.mozRequestFullScreen();
+  } else if (iframe.webkitRequestFullscreen) {
+    iframe.webkitRequestFullscreen();
+  } else if (iframe.msRequestFullscreen) {
+    iframe.msRequestFullscreen();
+  }
+}
+
+
+function abtblank() {
+  const url = location.href;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  let inFrame;
+
+  try {
+    inFrame = window !== top;
+  } catch (e) {
+    inFrame = true;
+  }
+
+  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = window.open("about:blank", name, `width=${width},height=${height}`);
+
+    if (!popup || popup.closed) {
+      alert("Allow popups and redirects to hide this from showing up in your history.");
+    } else {
+      const doc = popup.document;
+      const iframe = doc.createElement("iframe");
+      const style = iframe.style;
+      const link = doc.createElement("link");
+
+
+      iframe.src = url;
+      style.position = "fixed";
+      style.top = style.bottom = style.left = style.right = 0;
+      style.border = style.outline = "none";
+      style.width = style.height = "100%";
+
+      doc.head.appendChild(link);
+      doc.body.appendChild(iframe);
+      window.location.replace("https://google.com");
+    }
+  }
+}
+
+
+function iframeabtblank() {
+  const iframe = iframesContainer.children[currentTabIndex];
+  const url = iframe.src;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  let inFrame;
+
+  try {
+    inFrame = window !== top;
+  } catch (e) {
+    inFrame = true;
+  }
+
+  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = window.open("about:blank", name, `width=${width},height=${height}`);
+
+    if (!popup || popup.closed) {
+      alert("Allow popups and redirects to hide this from showing up in your history.");
+    } else {
+      const doc = popup.document;
+      const iframe = doc.createElement("iframe");
+      const style = iframe.style;
+      const link = doc.createElement("link");
+
+
+      iframe.src = url;
+      style.position = "fixed";
+      style.top = style.bottom = style.left = style.right = 0;
+      style.border = style.outline = "none";
+      style.width = style.height = "100%";
+
+      doc.head.appendChild(link);
+      doc.body.appendChild(iframe);
+      window.location.replace("https://google.com");
+    }
+  }
+}
+
+
+function exit() {
+  var confirm = window.confirm("Are you sure you want to close this tab?");
+  if (confirm) {
+      window.close();
+  }
+}
