c22efe61b8de4304ce4e02becea481492d457fc3
NottPeak <105991975+NottPeak@users.noreply.github.com>
2024-01-21 03:05:28 +0000

    arggg

--- public/settings/script.js
+++ public/settings/script.js
@@ -12,7 +12,7 @@ function abtblank() {
     }
   
     if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-      const popup = window.open("about:blank", name, `width=${width},height=${height}`);
+      const popup = window.open("about:blank", `width=${width},height=${height}`);
   
       if (!popup || popup.closed) {
         alert("Allow popups and redirects to hide this from showing up in your history.");
