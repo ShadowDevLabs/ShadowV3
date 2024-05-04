e97c7886559723f8921ed30b3e18aa76a9f409ca
NottPeak <105991975+NottPeak@users.noreply.github.com>
2024-05-04 09:03:19 +0000

    Why am i awake? ima just drop some of this down but half of its unfinished

--- public/sw.js
+++ public/sw.js
@@ -1,71 +1,22 @@
-//Thank you rifty :)
-importScripts("/uv/uv.bundle.js");
-importScripts("/uv/uv.config.js");
-importScripts(__uv$config.sw || "/uv/uv.sw.js");
-//import our IDB lib
-importScripts("/assets/localforage.js");
-localforage.config({
-  driver: localforage.INDEXEDDB,
-  name: "Shadow",
-  version: 1.0,
-  storeName: "shadow_config",
-  description: "Shadow config"
-});
+importScripts('/baremux/index.mjs')
+importScripts('/epoxy/index.js');
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts('/uv/uv.sw.js');
+importScripts('/dynamic/dynamic.config.js');
+importScripts('/dynamic/dynamic.client.js');
+importScripts('/dynamic/dynamic.worker.js');
+importScripts('/dynamic/dynamic.handler.js');
 
-// const dynPromise = new Promise(async (resolve) => {
-//   try {
-//     const bare =
-//       (await localforage.getItem("bare")) || location.origin + "/bare/";
-//     self.__dynamic$config.bare.path = bare;
-//     self.dynamic = new Dynamic(self.__dynamic$config);
-//   } catch (error) {
-//     console.log(error);
-//   }
-//   resolve();
-// });
+const uv = new UVServiceWorker();
 
-const uvPromise = new Promise(async (resolve) => {
-  try {
-    const bare =
-      (await localforage.getItem("bare")) || "https://phantom.lol/bare/";
-    const proxyUrl = (await localforage.getItem("HTTPProxy")) || "";
-    const [proxyIP, proxyPort] = proxyUrl.split(":");
-    self.__uv$config.bare = bare;
-    self.__uv$config.proxyPort = proxyPort;
-    self.__uv$config.proxyIp = proxyIP;
-    self.uv = new UVServiceWorker(self.__uv$config);
-  } catch (error) {
-    console.log(error);
-  }
-  resolve();
-});
-
-self.addEventListener("fetch", (event) => {
-  // if (
-  //   event.request.url.startsWith(location.origin + self.__dynamic$config.prefix)
-  // ) {
-  //   event.respondWith(
-  //     (async function () {
-  //       try {
-  //         await dynPromise;
-  //       } catch (error) {}
-  //       if (await self.dynamic.route(event)) {
-  //         return await self.dynamic.fetch(event);
-  //       }
-  //       await fetch(event.request);
-  //     })()
-  //   );
-  // } else 
-  if (
-    event.request.url.startsWith(location.origin + self.__uv$config.prefix)
-  ) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-      (async function () {
-        try {
-          await uvPromise;
-        } catch (error) {}
-        return await self.uv.fetch(event);
-      })()
+        (async ()=>{
+            if(event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+                return await uv.fetch(event);
+            }
+            return await fetch(event.request);
+        })()
     );
-  }
 });
