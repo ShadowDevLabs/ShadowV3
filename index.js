9f8e4a63beeb13d4f6a91311685daf1483e7d1d3
xNC <133831293+NCCoder2@users.noreply.github.com>
2024-09-07 04:32:26 +0000

    + Added New Theme, Pushed with Port 8080

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import { join } from "path";
 const version = process.env.npm_package_version;
 const publicPath = fileURLToPath(new URL("./public/", import.meta.url));
 const bare = createBareServer("/bare/");
-let port = 3000;
+let port = 8080;
 const app = express();
 const server = createServer();
 app.use(expressStaticGzip('./public/', {
