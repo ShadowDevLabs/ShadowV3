dc537fa27b1f0d5d760449ce51f75da0789bfa4f
Peak <peak@phantom.lol>
2024-10-14 02:49:21 -0500

    Custom user agent

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,18 @@
 importScripts("/uv/uv.bundle.js");
 importScripts("/uv/uv.config.js");
 importScripts(__uv$config.sw || "/uv/uv.sw.js");
-importScripts("/dynamic/dynamic.config.js");
-importScripts("/dynamic/dynamic.worker.js");
+importScripts("/assets/settings_manager_sw.js")
+self.settings = new SettingsManager();
 
 const uv = new UVServiceWorker();
-self.dynamic = new Dynamic(self.__dynamic$config);
+uv.on("request", async (event) => {
+  event.data.headers["user-agent"] = await self.settings.get("user-agent") ?? event.data.headers["user-agent"]
+});
 
-self.addEventListener("fetch", (event) => {
-  if (
-    event.request.url.startsWith(location.origin + self.__dynamic$config.prefix)
-  )
-    event.respondWith(
-      (async () => {
-        if (await self.dynamic.route(event))
-          return await self.dynamic.fetch(event);
 
-        await fetch(event.request);
-      })(),
-    );
-  else if (event.request.url.startsWith(location.origin + __uv$config.prefix))
+self.addEventListener("fetch", async (event) => {
+  if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
     event.respondWith((async () => await uv.fetch(event))());
+  }
   else event.respondWith((async () => await fetch(event.request))());
 });
