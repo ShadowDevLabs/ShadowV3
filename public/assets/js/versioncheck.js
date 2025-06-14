3e295172e08585dde26a2f25b5971a2e5089ca12
NottPeak <105991975+NottPeak@users.noreply.github.com>
2025-06-14 22:04:37 +0000

    oopsies (and future setup)

--- public/assets/js/versioncheck.js
+++ public/assets/js/versioncheck.js
@@ -4,7 +4,7 @@ const settings = new SettingsManager();
 
 onload = async () => {
   if (
-    (await fetch("/version")).headers["version"] !=
+    (await fetch("/v1/api/version")).headers["version"] !=
     await settings.get.getItem("version")
   )
     caches.keys().then((list) => list.map((key) => caches.delete(key)));
