d26499d00e182d7604feb758d4baa8e9f0cee6bb
xNC <nc.discord.acc@gmail.com>
2026-02-01 23:21:19 +0000

    some games?

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,8 +4,10 @@ const modal = document.getElementById("modal");
 const closeButton = document.querySelector(".close");
 
 const defaultShortcuts = [
-  { name: "Roblox", url: "https://nowgg.fun/apps/a/19900/b.html", icon: "assets/imgs/icons/apps/2.png"},
-  { name: "Now.GG", url: "https://nowgg.fun/", icon: "assets/imgs/icons/apps/1.png" },
+  { name: "Roblox", url: "https://astra.pxi-fusion.com/embed/shadow/roblox", icon: "assets/imgs/icons/apps/2.png"},
+  { name: "Clash Royale", url: "https://astra.pxi-fusion.com/embed/shadow/clash-royale", icon: "assets/imgs/icons/apps/5.webp"},
+  { name: "Fortnite", url: "https://astra.pxi-fusion.com/embed/shadow/fortnite", icon: "assets/imgs/icons/apps/4.jpg"},
+  { name: "Brawl Stars", url: "https://astra.pxi-fusion.com/embed/shadow/brawl-stars", icon: "assets/imgs/icons/apps/6.jpg"},
   { name: "G@me$", url: "shadow://books", icon: "assets/imgs/icons/apps/3.png" }
 ];
 
