21b3e373231ed1115883af96118ca6b49b70d004
Trevor Tesch <tdltesch@gmail.com>
2026-08-17 14:14:26 -0400

    Add panic key redirect to safe site
    Implement panic key functionality to redirect to a safe site.

--- public/assets/js/panic.js
+++ public/assets/js/panic.js
@@ -1,1 +1,19 @@
-//remaking bc it was bad
+// public/assets/js/panic.js
+
+(function() {
+    const safeSite = 'https://google.com';
+    const panicKeyCode = 'Equal';
+
+    function redirectToSafeSite() {
+        window.location.replace(safeSite);
+    }
+
+    window.addEventListener('keydown', (event) => {
+        if (event.code === panicKeyCode && !event.altKey && !event.ctrlKey && !event.shiftKey && !event.metaKey) {
+            event.preventDefault();
+            redirectToSafeSite();
+        }
+    });
+
+    console.log('Panic key active: Press the equal key to hide this page.');
+})();
